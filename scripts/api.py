d7b4f845bb7a9f356c373da851ca0427319d145e
NotNite <hi@notnite.com>
2024-05-18 21:52:36 -0400

    Ghidra support

--- scripts/api.py
+++ scripts/api.py
@@ -27,8 +27,12 @@ class BaseApi(object):
         # type: (int) -> str
         pass
 
-    def set_name(self, address, name):
-        # type: (int, str) -> bool
+    def set_name(self, address, name, type):
+        # type: (int, str, str) -> bool
+        pass
+
+    def read_u32(self, address):
+        # type: (int) -> int
         pass
 
     def read_u64(self, address):
@@ -75,7 +79,7 @@ try:
 
             if signature.startswith("E8") or signature.startswith("E9"):
                 # Displacement
-                offset = ida_bytes.get_wide_dword(addr + 1)
+                offset = self.read_u32(addr + 1)
                 offset = ~0x7FFFFFFF | offset  # convert to signed
                 addr += 5 + offset
 
@@ -83,25 +87,26 @@ try:
 
         def get_instructions(self, address):
             func = ida_funcs.get_func(address)
-            if func is None:
-                return []
-
-            insns = list(idautils.FuncItems(func.start_ea))
-            for insn_addr in insns:
-                insn = ida_ua.insn_t()
-                ida_ua.decode_insn(insn, insn_addr)
-                yield Instruction(
-                    insn_addr,
-                    insn.get_canon_mnem(),
-                    list(op.addr for op in insn.ops),
-                    list(op.value for op in insn.ops),
-                )
+            if func is not None:
+                insns = list(idautils.FuncItems(func.start_ea))
+                for insn_addr in insns:
+                    insn = ida_ua.insn_t()
+                    ida_ua.decode_insn(insn, insn_addr)
+                    yield Instruction(
+                        insn_addr,
+                        insn.get_canon_mnem(),
+                        list(op.addr for op in insn.ops),
+                        list(op.value for op in insn.ops),
+                    )
 
         def get_name(self, address):
             return ida_funcs.get_func_name(address)
 
-        def set_name(self, address, name):
+        def set_name(self, address, name, type):
             return ida_name.set_name(address, name, ida_name.SN_FORCE)
+
+        def read_u32(self, address):
+            return ida_bytes.get_wide_dword(address)
 
         def read_u64(self, address):
             return ida_bytes.get_qword(address)
@@ -187,17 +192,140 @@ try:
     api = IDAApi()
 except ImportError:
     import ghidra
+    from ghidra.program.model.address import Address
+
+    from __main__ import *  # What the hell Ghidra
+
+    # Create a fake typing to appease Pylance *and* Python 2.7
+    class FakeTyping(object):
+        def __getattr__(self, name):
+            return None
+
+    typing = None
+    try:
+        import typing
+    except ImportError:
+        typing = FakeTyping()
 
     # https://github.com/VDOO-Connected-Trust/ghidra-pyi-generator
-    # ...but the types still seem a little broken
-    try:
-        from typing import List
+    if typing.TYPE_CHECKING:
         from ghidra.ghidra_builtins import *
-    except:
-        pass
 
     # todo lmao
     class GhidraApi(BaseApi):
-        pass
+        def scan(self, signature):
+            # Turn byte into \x00 and ?? into .
+            str = ""
+            chunks = signature.split(" ")
+            for chunk in chunks:
+                if chunk == "?" or chunk == "??":
+                    str += "."
+                else:
+                    str += "\\x" + chunk
+
+            results = findBytes(None, str, 1)
+            if len(results) == 0:
+                return None
+
+            addr = results[0].getOffset()
+
+            if signature.startswith("E8") or signature.startswith("E9"):
+                offset = self.read_u32(addr + 1)
+                offset = ~0x7FFFFFFF | offset  # convert to signed
+                addr += 5 + offset
+
+            return addr
+
+        def get_instructions(self, address):
+            listing = currentProgram.getListing()
+            address_factory = currentProgram.getAddressFactory()
+            address_space = address_factory.getDefaultAddressSpace()
+            codeUnit = listing.getCodeUnitAt(address_space.getAddress(address))
+            while codeUnit is not None:
+                if codeUnit.getMnemonicString() == "RET":
+                    break
+
+                operands = []
+                for i in range(codeUnit.getNumOperands()):
+                    for o in codeUnit.getOpObjects(i):
+                        if isinstance(o, ghidra.program.model.address.Address):
+                            operands.append(o.getOffset())
+                        elif isinstance(o, ghidra.program.model.scalar.Scalar):
+                            operands.append(o.getUnsignedValue())
+                        elif isinstance(o, ghidra.program.model.symbol.Reference):
+                            operands.append(o.getToAddress().getOffset())
+
+                yield Instruction(
+                    codeUnit.getMinAddress().getOffset(),
+                    codeUnit.getMnemonicString().lower(),
+                    # idk how this api works lol
+                    operands,
+                    operands,
+                )
+
+                codeUnit = listing.getInstructionAfter(codeUnit.getAddress())
+
+        def set_name(self, address, name, type):
+            symbol_table = currentProgram.getSymbolTable()
+            address_factory = currentProgram.getAddressFactory()
+            address_space = address_factory.getDefaultAddressSpace()
+            addr = address_space.getAddress(address)
+
+            # Ghidra is bad and some of these are UndefinedFunctions
+            function = getFunctionAt(addr)
+            if function is not None:
+                function.setName(
+                    name, ghidra.program.model.symbol.SourceType.USER_DEFINED
+                )
+            elif type == "function" and function is None:
+                # Make the function for Ghidra since it's bad
+                function = createFunction(addr, name)
+            else:
+                symbol_table.createLabel(
+                    addr, name, ghidra.program.model.symbol.SourceType.USER_DEFINED
+                )
+
+            return True
+
+        def read_str(self, address):
+            address_factory = currentProgram.getAddressFactory()
+            address_space = address_factory.getDefaultAddressSpace()
+            addr = address_space.getAddress(address)
+            str = ""
+            while True:
+                byte = getByte(addr)
+                if byte == 0:
+                    break
+                str += chr(byte)
+                addr = addr.add(1)
+
+            return str
+
+        def read_bytes(self, address, size):
+            address_factory = currentProgram.getAddressFactory()
+            address_space = address_factory.getDefaultAddressSpace()
+            addr = address_space.getAddress(address)
+            bytes = getBytes(addr, size)
+
+            # Convert to unsigned
+            new_bytes = []
+            for i in range(size):
+                if bytes[i] < 0:
+                    new_bytes.append(bytes[i] + 256)
+                else:
+                    new_bytes.append(bytes[i])
+
+            return new_bytes
+
+        def read_u32(self, address):
+            bytes = self.read_bytes(address, 4)
+            return bytes[0] | (bytes[1] << 8) | (bytes[2] << 16) | (bytes[3] << 24)
+
+        def read_u64(self, address):
+            one = self.read_u32(address)
+            two = self.read_u32(address + 4)
+            return one | (two << 32)
+
+        # TODO type args
 
     api = GhidraApi()
--- scripts/rename.py
+++ scripts/rename.py
@@ -12,21 +12,21 @@ from api import api
 
 def handle_vars(addr):
     global api
-    print(f"handle_vars: {addr:X}")
+    print("handle_vars: {:02X}".format(addr))
     while True:
         variable = api.read_u64(addr)
         if variable == 0:
             break
 
         string = api.read_str(api.read_u64(variable))
-        api.set_name(variable + 8, f"var_{string}")
+        api.set_name(variable + 8, "var_" + string, None)
 
         addr += 8
 
 
 def handle_funcs(addr):
     global api
-    print(f"handle_funcs: {addr:X}")
+    print("handle_funcs: {:02X}".format(addr))
     while True:
         func_addr = api.read_u64(addr)
         if func_addr == 0:
@@ -39,14 +39,14 @@ def handle_funcs(addr):
         func_name = api.read_str(func_name_addr)
 
         reference = func_addr + 8
-        api.set_name(reference, f"func_{func_name}")
+        api.set_name(reference, "func_" + func_name, None)
 
         addr += 8
 
 
 def handle_gml_funcs(addr, num_funcs):
     global api
-    print(f"handle_gml_funcs: {addr:X}")
+    print("handle_gml_funcs: {:02X}".format(addr))
     i = 0
     while True:
         if i >= num_funcs:
@@ -62,11 +62,11 @@ def handle_gml_funcs(addr, num_funcs):
             break
 
         func = api.read_u64(addr + 8)
-        api.set_name(func, func_name)
+        api.set_name(func, func_name, "function")
 
         # is this correct?
         func_ref = api.read_u64(addr + 16) + 8
-        api.set_name(func_ref, f"funcref_{func_name}")
+        api.set_name(func_ref, "funcref_" + func_name, None)
 
         api.set_func_arg_type(func, 0, "CInstance", 1, "self")
         api.set_func_arg_type(func, 1, "CInstance", 1, "other")
@@ -83,17 +83,29 @@ def parse_init_llvm():
     init_llvm = api.scan("E8 ?? ?? ?? ?? 48 8B 0D ?? ?? ?? ?? 8B 41 14")
     if init_llvm is None:
         return
-    print(f"init_llvm: {init_llvm:X}")
+    print("init_llvm: {:02X}".format(init_llvm))
 
     insns = list(api.get_instructions(init_llvm))
     for insn in insns:
+        if len(insn.operands) < 1:
+            continue
+
         if insn.opcode == "mov":
             offset = insn.operands[0]
             if offset == 0x18 or offset == 0x20 or offset == 0x28:
                 for i in range(insns.index(insn) - 1, -1, -1):
                     insn2 = insns[i]
+                    if len(insn2.operands) < 1:
+                        continue
+
                     if insn2.opcode == "lea":
-                        addr = insn2.operands[1]
+                        # Shitty Ghidra hack
+                        addr = None
+                        if len(insn2.operands) == 1:
+                            addr = insn2.operands[0]
+                        else:
+                            addr = insn2.operands[1]
+
                         if offset == 0x18:
                             handle_vars(addr)
                         if offset == 0x20:
@@ -105,7 +117,7 @@ def parse_init_llvm():
                                     insns[j].opcode == "mov"
                                     and insns[j].operands[0] == 0x14
                                 ):
-                                    num_funcs = insns[j].values[0]
+                                    num_funcs = insns[j].values[1]
                                     break
 
                             handle_gml_funcs(addr, num_funcs)
